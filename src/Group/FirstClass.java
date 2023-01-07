e32472ca76f547880c97cf90768a5bc1c90f8e95
Erumkaleemullah <114360536+Erumkaleemullah@users.noreply.github.com>
2023-01-06 20:17:39 -0600

    Update FirstClass.java

--- src/Group/FirstClass.java
+++ src/Group/FirstClass.java
@@ -3,5 +3,7 @@ package Group;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("Hi");
+        System.out.println("hello);
+            
     }
 }
